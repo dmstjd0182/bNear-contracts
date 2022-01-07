a9c7fdd06fada6d3f3f5532d5725b3bd3d77fded
dmstjd0182 <dmstjd0182@snu.ac.kr>
2022-01-07 17:01:47 +0900

    (views) impl:  get_total_supply

---
+++ contracts/basset_hub/src/views.rs
@@ -0,0 +1,12 @@
+use near_sdk::{near_bindgen};
+use near_sdk::json_types::{U128};
+
+use crate::*;
+use crate::utils::{ext_fungible_token};
+
+#[near_bindgen]
+impl Contract {
+    pub fn get_total_supply() -> U128 {
+        ext_fungible_token::ft_total_supply()
+    }
+}
